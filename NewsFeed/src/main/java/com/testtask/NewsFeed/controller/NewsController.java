189073a9153b987853f7522db6f3789522ef352b
LexSolo <saleksandr950@gmail.com>
2020-03-03 16:21:59 +0700

    getListOfAllNews() method was added to controller.

--- NewsFeed/src/main/java/com/testtask/NewsFeed/controller/NewsController.java
+++ NewsFeed/src/main/java/com/testtask/NewsFeed/controller/NewsController.java
@@ -1,10 +1,17 @@
 package com.testtask.NewsFeed.controller;
 
+import com.testtask.NewsFeed.model.NewsDTO;
 import com.testtask.NewsFeed.service.NewsService;
+import com.testtask.NewsFeed.utils.AddressConstants;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping(AddressConstants.DEFAULT_ADDRESS)
 public class NewsController {
 
     private NewsService newsService;
@@ -14,5 +21,8 @@ public class NewsController {
         this.newsService = newsService;
     }
 
-
+    @GetMapping("10/")
+    public List<NewsDTO> getListOfAllNews() {
+        return newsService.getListOfAllNews();
+    }
 }
--- NewsFeed/src/main/java/com/testtask/NewsFeed/service/NewsService.java
+++ NewsFeed/src/main/java/com/testtask/NewsFeed/service/NewsService.java
@@ -1,8 +1,12 @@
 package com.testtask.NewsFeed.service;
 
+import com.testtask.NewsFeed.model.NewsDTO;
 import com.testtask.NewsFeed.repository.NewsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class NewsService {
@@ -14,5 +18,14 @@ public class NewsService {
         this.newsRepository = newsRepository;
     }
 
+    public List<NewsDTO> getListOfAllNews() {
+        Iterable<NewsDTO> newsFromDB = newsRepository.findAll();
 
+        List<NewsDTO> newsList = new ArrayList<>();
+        for (NewsDTO news : newsFromDB) {
+            newsList.add(news);
+        }
+
+        return newsList;
+    }
 }
