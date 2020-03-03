a6056eca333c30327af240a7b70c258cd23255f7
LexSolo <saleksandr950@gmail.com>
2020-03-03 17:07:27 +0700

    Connection to database was configured.

--- NewsFeed/src/main/java/com/testtask/NewsFeed/NewsFeedApplication.java
+++ NewsFeed/src/main/java/com/testtask/NewsFeed/NewsFeedApplication.java
@@ -1,5 +1,6 @@
 package com.testtask.NewsFeed;
 
+import com.testtask.NewsFeed.config.JpaConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NewsFeedApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NewsFeedApplication.class, args);
+		SpringApplication.run(new Class[] {NewsFeedApplication.class, JpaConfig.class}, args);
 	}
 
 }
