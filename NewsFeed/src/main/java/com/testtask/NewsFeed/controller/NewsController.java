b1b6c1c74bcf5e712d4544af54b38bb8685c36b5
LexSolo <saleksandr950@gmail.com>
2020-03-03 15:52:22 +0700

    NewsController, NewsService and NewsRepository were linked together with the @Autowired annotations.

--- NewsFeed/src/main/java/com/testtask/NewsFeed/controller/NewsController.java
+++ NewsFeed/src/main/java/com/testtask/NewsFeed/controller/NewsController.java
@@ -1,9 +1,18 @@
 package com.testtask.NewsFeed.controller;
 
+import com.testtask.NewsFeed.service.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NewsController {
+
+    private NewsService newsService;
+
+    @Autowired
+    public NewsController(NewsService newsService) {
+        this.newsService = newsService;
+    }
 
 
 }
--- NewsFeed/src/main/java/com/testtask/NewsFeed/service/NewsService.java
+++ NewsFeed/src/main/java/com/testtask/NewsFeed/service/NewsService.java
@@ -1,7 +1,18 @@
 package com.testtask.NewsFeed.service;
 
+import com.testtask.NewsFeed.repository.NewsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NewsService {
+
+    private NewsRepository newsRepository;
+
+    @Autowired
+    public NewsService(NewsRepository newsRepository) {
+        this.newsRepository = newsRepository;
+    }
+
+
 }
