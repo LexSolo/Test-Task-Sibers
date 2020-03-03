54345883c6ee9fb994e0fd24fc7e2c66581416e6
LexSolo <saleksandr950@gmail.com>
2020-03-03 15:42:18 +0700

    NewsDTO class was designed.

---
+++ NewsFeed/src/main/java/com/testtask/NewsFeed/model/NewsDTO.java
@@ -0,0 +1,62 @@
+package com.testtask.NewsFeed.model;
+
+import javax.persistence.*;
+import java.net.URL;
+import java.util.Calendar;
+
+@Entity
+@Table(name = "news")
+public class NewsDTO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String header;
+    private Calendar publishingDate;
+    private String newsBody;
+    private URL image;
+
+    public NewsDTO() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public Calendar getPublishingDate() {
+        return publishingDate;
+    }
+
+    public void setPublishingDate(Calendar publishingDate) {
+        this.publishingDate = publishingDate;
+    }
+
+    public String getNewsBody() {
+        return newsBody;
+    }
+
+    public void setNewsBody(String newsBody) {
+        this.newsBody = newsBody;
+    }
+
+    public URL getImage() {
+        return image;
+    }
+
+    public void setImage(URL image) {
+        this.image = image;
+    }
+}
