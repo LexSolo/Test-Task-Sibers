ae5d5d9dc884ba0eb86dd17d527a1ebe2b43d8de
LexSolo <saleksandr950@gmail.com>
2020-03-04 17:08:00 +0700

    Fixes of application.properties file. Application is alive now.

--- NewsFeed/src/main/java/com/testtask/NewsFeed/NewsFeedApplication.java
+++ NewsFeed/src/main/java/com/testtask/NewsFeed/NewsFeedApplication.java
@@ -1,6 +1,6 @@
 package com.testtask.NewsFeed;
 
-import com.testtask.NewsFeed.config.JpaConfig;
+//import com.testtask.NewsFeed.config.JpaConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NewsFeedApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(new Class[] {NewsFeedApplication.class, JpaConfig.class}, args);
+//		SpringApplication.run(new Class[] {NewsFeedApplication.class, JpaConfig.class}, args);
+		SpringApplication.run(NewsFeedApplication.class, args);
 	}
 
 }
