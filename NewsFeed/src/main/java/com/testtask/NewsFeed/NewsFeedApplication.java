b8e806c1e68a49bf47c224dc1071393fe69a8348
LexSolo <saleksandr950@gmail.com>
2020-03-04 17:10:24 +0700

    Proceeding of configuration of DB.

--- NewsFeed/src/main/java/com/testtask/NewsFeed/NewsFeedApplication.java
+++ NewsFeed/src/main/java/com/testtask/NewsFeed/NewsFeedApplication.java
@@ -1,6 +1,5 @@
 package com.testtask.NewsFeed;
 
-//import com.testtask.NewsFeed.config.JpaConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NewsFeedApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(new Class[] {NewsFeedApplication.class, JpaConfig.class}, args);
 		SpringApplication.run(NewsFeedApplication.class, args);
 	}
 
