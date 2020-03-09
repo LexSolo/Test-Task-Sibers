26b87f4a5cc8732b3cb706dae3b840a08ae89af3
LexSolo <saleksandr950@gmail.com>
2020-03-09 21:50:26 +0700

    Docs for NewsController were added.

--- NewsFeed/src/main/java/com/testtask/NewsFeed/controller/NewsController.java
+++ NewsFeed/src/main/java/com/testtask/NewsFeed/controller/NewsController.java
@@ -9,28 +9,56 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// indicates the RESTful app for Spring and the Controller class at the same time
 @RestController
+// the default address for appealing to the app
 @RequestMapping(AddressConstants.DEFAULT_ADDRESS)
+/**
+ * I'd like to add project Lombok annotations to make the dependency injection easier.
+ * Instead of writing @Autowired annotation near the constructor below I could write @RequiredArgsConstructor annotation,
+ * and do without newsService field injection further.
+ * However, it is the problems possible to use this library on your computer without installing required components.
+ * So that, I refused to use Lombok.
+ *
+ * This is the simple controller class below.
+ * */
 public class NewsController {
 
+    // the field for connection wih the service
     private NewsService newsService;
 
+    // dependency injection by the constructor with newsService field
     @Autowired
     public NewsController(NewsService newsService) {
         this.newsService = newsService;
     }
 
+    /**
+     * The simplest GET mapping.
+     *
+     * @return String of greeting at the default address.
+     * */
     @GetMapping
     public String showGreeting() {
         return newsService.showGreeting();
     }
 
-    @GetMapping("10/")
+    /**
+     * Returns list of news.
+     *
+     * @return List of NewsDto elements.
+     * */
+    @GetMapping(value = "allNews/")
     public List<NewsDTO> getListOfAllNews() {
         return newsService.getListOfAllNews();
     }
 
-    @PostMapping("add-news/")
+    /**
+     * Checks if the news with the header exists and returns the http code.
+     *
+     * @return ResponseEntity class with success http code if the news exists else error http code.
+     * */
+    @PostMapping(value = "add-news/")
     public ResponseEntity<?> addNews(@RequestBody NewsDTO newsDTO) {
         if (!newsService.addNews(newsDTO)) {
             System.out.println("Error. The news with this header exists. Nothing to add.");
