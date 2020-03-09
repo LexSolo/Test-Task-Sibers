210030bfa0df0cb1caf269930ef47404b036ae02
LexSolo <saleksandr950@gmail.com>
2020-03-09 22:03:12 +0700

    Docs for NewsService class were added.

--- NewsFeed/src/main/java/com/testtask/NewsFeed/service/NewsService.java
+++ NewsFeed/src/main/java/com/testtask/NewsFeed/service/NewsService.java
@@ -8,20 +8,37 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The simplest service class for all logic operations with the data.
+ * */
+
+// annotation marks class as Service to get easy Spring application working with
 @Service
 public class NewsService {
 
+    // the field for connection wih the repository or database
     private NewsRepository newsRepository;
 
+    // dependency injection by the constructor with newsService field
     @Autowired
     public NewsService(NewsRepository newsRepository) {
         this.newsRepository = newsRepository;
     }
 
+    /**
+     * The simplest method for String returning.
+     *
+     * @return String of greeting at the default address.
+     * */
     public String showGreeting() {
         return "Welcome!";
     }
 
+    /**
+     * Returns list of news.
+     *
+     * @return List of NewsDto elements from database.
+     * */
     public List<NewsDTO> getListOfAllNews() {
         Iterable<NewsDTO> newsFromDB = newsRepository.findAll();
 
@@ -33,10 +50,18 @@ public class NewsService {
         return newsList;
     }
 
+    /**
+     * Checks if the news with the header exists and returns the boolean value.
+     *
+     * @param newsDTO is the transformed news from database to lava class.
+     * @return true if the news exists and saves if in the database else false with the message.
+     * */
     public boolean addNews(NewsDTO newsDTO) {
         for (NewsDTO news : getListOfAllNews()) {
-            if (news.getHeader().equals(newsDTO.getHeader()))
+            if (news.getHeader().equals(newsDTO.getHeader())) {
+                System.out.println("Error. The news with this header exists. Nothing to add.");
                 return false;
+            }
         }
 
         newsRepository.save(newsDTO);
