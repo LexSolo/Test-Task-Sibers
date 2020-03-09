7b599cd35dc6e18baebe08707ac9ad4614f5609f
LexSolo <saleksandr950@gmail.com>
2020-03-09 21:54:37 +0700

    Docs for AddressConstants were added.

--- NewsFeed/src/main/java/com/testtask/NewsFeed/utils/AddressConstants.java
+++ NewsFeed/src/main/java/com/testtask/NewsFeed/utils/AddressConstants.java
@@ -1,9 +1,18 @@
 package com.testtask.NewsFeed.utils;
 
+/**
+ * The util class for storing constants.
+ * At this case it is the default address of the app.
+ * */
+
 public final class AddressConstants {
 
+    // the constructor for correct Spring Boot app working.
+    // I could use the @NoArgsConstructor annotation from Lombok library, but I refused to use Lombok.
+    // You could find out why in the NewsController class' docs.
     private AddressConstants() {
     }
 
+    // default address of the app
     public static final String DEFAULT_ADDRESS = "/";
 }
