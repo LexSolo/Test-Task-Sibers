c9e96d887afd40429411935b1c2fed8ff192c87c
LexSolo <saleksandr950@gmail.com>
2020-03-09 22:11:54 +0700

    Docs for NewsFeedApplication class were added.

--- NewsFeed/src/main/java/com/testtask/NewsFeed/NewsFeedApplication.java
+++ NewsFeed/src/main/java/com/testtask/NewsFeed/NewsFeedApplication.java
@@ -3,6 +3,13 @@ package com.testtask.NewsFeed;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main class of the Spring Boot application to start with.
+ *
+ * @author Aleksandr Solovev.
+ * */
+
+// annotation marks class as an entry point of the app
 @SpringBootApplication
 public class NewsFeedApplication {
 
