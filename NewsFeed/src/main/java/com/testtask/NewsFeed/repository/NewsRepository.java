721269b4fc7f7aec5163033768e58eabc9b860c6
LexSolo <saleksandr950@gmail.com>
2020-03-09 22:10:19 +0700

    Docs for NewsRepository interface were added.

--- NewsFeed/src/main/java/com/testtask/NewsFeed/repository/NewsRepository.java
+++ NewsFeed/src/main/java/com/testtask/NewsFeed/repository/NewsRepository.java
@@ -6,7 +6,13 @@ import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
+/**
+ * The simplest repository class for all calling operations to the database.
+ * The repository extends CrudRepository for extending it's properties and adding new if necessary.
+ * */
+// annotation marks class as Repository to get easy Spring application working with
 @Repository
+// annotation for automatic generation objects for database-java connection
 @Transactional
 public interface NewsRepository extends CrudRepository<NewsDTO, Long> {
 }
