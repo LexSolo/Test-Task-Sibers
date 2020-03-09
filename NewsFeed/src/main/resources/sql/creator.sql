198d4e07707b36494a52fe32538986c865a06367
LexSolo <saleksandr950@gmail.com>
2020-03-09 21:19:43 +0700

    Docs for creator.sql were added.

--- NewsFeed/src/main/resources/sql/creator.sql
+++ NewsFeed/src/main/resources/sql/creator.sql
@@ -1,7 +1,8 @@
+-- creates the 'news' table
 CREATE TABLE news(
-    ID SERIAL NOT NULL PRIMARY KEY,
-    Header VARCHAR(250) NOT NULL,
-    Date DATETIME_INTERVAL_CODE NOT NULL,
-    Text MESSAGE_TEXT NOT NULL
-    Image BYTEA
+    ID SERIAL NOT NULL PRIMARY KEY, -- unique identification number for every news
+    Header VARCHAR(250) NOT NULL, -- header of news
+    Date DATETIME_INTERVAL_CODE NOT NULL, -- publishing date
+    Text MESSAGE_TEXT NOT NULL -- news' text
+    Image BYTEA -- news' image, optional parameter
 );
